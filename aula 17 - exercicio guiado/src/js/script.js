e0665f999fcecc88397c962e467c2d3f5827e956
Rafael dos Santos Silveira <rafaelssilveira@outlook.com>
2020-05-28 17:26:13 -0300

    até aula 17.3

--- aula 17 - exercicio guiado/src/js/script.js
+++ aula 17 - exercicio guiado/src/js/script.js
@@ -13,16 +13,100 @@ window.addEventListener('load',()=>{
     tabCountries= document.querySelector('#tabCountries')
     tabFavorites= document.querySelector('#tabFavorites')
     countCountries= document.querySelector('#countCountries')
-    countFavorites= document.querySelector('#countCountries')
+    countFavorites= document.querySelector('#countFavorites')
 
     totalPopulationList=document.querySelector("#totalPopulationList") 
     totalPopulationFavorites=document.querySelector("#totalPopulationFavorites") 
 
-    numberFormat= Intl.numberFormat('pt-BR')
+    numberFormat= Intl.NumberFormat('pt-BR')
 
-    favoritesCountries();
+    fetchCountries();
 })    
-function favoritesCountries(){
-    console.log("rodando..");
+async function fetchCountries(){
+    const res = await fetch('https://restcountries.eu/rest/v2/all')
+    const json = await res.json()
+    allCountries= json.map(country=>{
+        const {numericCode,translations,population,flag }= country;
+        return {
+           id:numericCode,
+           name:translations.pt,
+           population,
+           flag
+        }
+    })
+    favoritesCountries = allCountries 
+    render();
     
 }
+function render(){
+    renderCountryList()
+    renderFavorites()
+    renderSummary()
+
+    handleCountryButtons()
+}
+function renderCountryList(){
+   let countriesHTML="<div>"
+   allCountries.forEach(country =>{
+       const {name,flag,id,population}= country;
+       const countryHTML= `
+        <div class="country">
+            <div>
+            <a id="${id}" class="waves-effect waves-light btn">+</a>
+            </div>
+            <div>
+            <img src="${flag}" alt="${name}">
+            </div>
+            <div>
+            <ul>
+                <li>${name}</li>
+                <li>${population}</li>
+            </ul>
+            </div>
+        </div>
+       `;
+       countriesHTML += countryHTML;
+   }); 
+   countriesHTML += '</div>'
+   tabCountries.innerHTML= countriesHTML   
+}
+function renderFavorites(){
+    let favoritesHTML = '<div>'
+        favoritesCountries.forEach(country=>{
+            const {name,flag,id,population}= country;
+            const favoriteCountryHTML= `
+            <div class="country">
+                <div>
+                   <a id="${id}" class="waves-effect waves-light btn red darken-4">+</a>
+                </div>
+                <div>
+                  <img src="${flag}" alt="${name}">
+                </div>
+                <div>
+                <ul>
+                    <li>${name}</li>
+                    <li>${population}</li>
+                </ul>
+                </div>
+            </div>
+           `;
+           favoritesHTML += favoriteCountryHTML;
+
+        })
+    favoritesHTML += '</div>'
+    tabFavorites.innerHTML= favoritesHTML
+}
+function renderSummary(){
+    countCountries.textContent = allCountries.length
+    countFavorites.textContent = favoritesCountries.length
+
+    const totalPopulation = allCountries.reduce((accumulator,current)=>{
+        return accumulator + current.population
+    },0)
+    const totalFavorites = favoritesCountries.reduce((accumulator,current)=>{
+        return accumulator + current.population
+    },0)
+    totalPopulationList.textContent=totalPopulation
+    totalPopulationFavorites.textContent=totalFavorites
+}
+function handleCountryButtons(){}
