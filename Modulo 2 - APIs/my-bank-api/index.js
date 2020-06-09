4ce8d5f62cca7b462ae4bb5008648c28e59b0420
Rafael Silveira <rafaelssilveira@outlook.com>
2020-06-09 19:40:25 -0300

    foi

--- Modulo 2 - APIs/my-bank-api/index.js
+++ Modulo 2 - APIs/my-bank-api/index.js
@@ -1,63 +1,30 @@
 var express = require("express");
 var app = express();
+var accountsRouter = require('./routes/accounts.js')
 var fs = require("fs");
+
+global.fileName="accounts.json"
 // dizendo para o express que ira receber requisições JSON
 app.use(express.json());
+//sempre que chegar nesse endereço sera encaminhado para o arquivo accounts
+app.use('/account',accountsRouter)
 
 app.get("/", (req, res) => {
   res.send("hello wolrd");
 });
 
-app.post("/account", (req, res) => {
-  let account = req.body;
-  fs.readFile("accounts.json", "utf8", (err, data) => {
-    if (!err) {
-      try {
-        let json = JSON.parse(data);
-        account = { id: json.nextId++, ...account };
-        json.accounts.push(account);
-        fs.writeFile("accounts.json", JSON.stringify(json), (err) => {
-          if (err) {
-            res.status(400).send({ error: err.message });
-          } else {
-            res.end();
-          }
-        });
-      } catch (err) {
-        res.status(400).send({ error: err.message });
-      }
-    } else {
-      res.status(400).send({ error: err.message });
-    }
-  });
-
-  return res.send("teste");
-});
-//teste
-
-app.get("/account", (_, res) => {
-  fs.readFile("accounts.json", "utf8", (err, data) => {
-    if (!err) {
-      let json = JSON.parse(data);
-      delete json.nextId;
-      res.send(json);
-    } else {
-      res.status(400).send({ error: err.message });
-    }
-  });
-});
 
 app.listen(3000, () => {
   // verificar se o arquivo de com as contas já existe(acconts.json)
   // usando try catch para não quebrar a aplicação
   try {
-    fs.readFile("accounts.json", "utf8", (err, data) => {
+    fs.readFile(global.fileName, "utf8", (err, data) => {
       if (err) {
         const initialJson = {
           nextId: 1,
           accounts: [],
         };
-        fs.writeFile("accounts.json", JSON.stringify(initialJson), (err) => {
+        fs.writeFile(global.fileName, JSON.stringify(initialJson), (err) => {
           if (err) {
             console.log(err);
           }
