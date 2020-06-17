c12e6c325b782512e4613fe45239e514b1f1739b
root <rafaelssilveira@outlook.com>
2020-06-17 23:20:54 +0000

    codigos ate a aula 7

--- Modulo 3 - React/aula-05/react-counter-01/src/components/Counter/Counter.js
+++ Modulo 3 - React/aula-05/react-counter-01/src/components/Counter/Counter.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+// Para usar o css, criamos um arquivo no mesmo diretorio desse arquivo e importamos,
+// é importante usar o nomeArquivo.module.css para o react entender o modulos
 
 import css from './counter.module.css';
 
 export default class Counter extends Component {
+// para usar o Component temos que usar o constructor junto com o super
   constructor() {
     super();
-
+// Aqui estamos criando atributos, que não sao muito comum em react, normalmente se usa o state que será explicado em aula futuras // para criar atributos é obrigátorio o uso de 'this'
     this.currentCounter = 2;
   }
 
@@ -17,19 +20,18 @@ export default class Counter extends Component {
 
   render() {
     return (
+		// no JSX é alguns atributos os nomes foram modificados, por isso usamos className ao inves de class
+		// O que está em {} são códigos JS dentro do JSX 
+		// Para usar o css, bastat usar o nome de como importamos o css (css) e o nome da classe 
       <div className={css.counterContainer}>
         <button
           onClick={this.handleClick}
-          className="waves-effect 
-   waves-light btn red darken-4"
-        >
+          className="waves-effect waves-light btn red darken-4">
           -
         </button>
         <span className={css.counterValue}>{this.currentCounter}</span>
         <button
-          className="waves-effect 
-   waves-light btn green darken-4"
-        >
+          className="waves-effect waves-light btn green darken-4"   >
           +
         </button>
       </div>
--- Modulo 3 - React/aula-06/react-counter-01/src/App.js
+++ Modulo 3 - React/aula-06/react-counter-01/src/App.js
@@ -1,16 +1,13 @@
-// sem chaves são modulos principais e com chaves são modulos auxiliares
 import React, { Component, Fragment } from 'react';
 import Counter from './components/Counter/Counter';
 
-// A classe que estamos criando App, estamos expotando para ser utilizada em outros arquivos,
-// Ao mesmo tempo que estamos importando modulos do Component para essa classe
 export default class App extends Component {
   render() {
     return (
       <Fragment>
         <Counter />
         <Counter />
-        <Counter />
+        
       </Fragment>
 		
 		
